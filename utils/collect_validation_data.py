f453f47de4e1033f9fce6d26c165a40626e296a8
Michael Dales <mwd24@cam.ac.uk>
2024-12-11 10:40:10 +0000

    add script to pull out validation outliers

---
+++ utils/collect_validation_data.py
@@ -0,0 +1,55 @@
+import argparse
+import os
+import shutil
+
+import pandas as pd
+
+def collect_validation_data(
+    model_results_path: str,
+    data_dir: str,
+    output_dir: str,
+) -> None:
+    model_results = pd.read_csv(model_results_path)
+    os.makedirs(output_dir, exist_ok=True)
+
+    outliers = model_results[model_results.outlier is True]
+    for _, row in outliers.iterrows():
+        taxid = row.id_no
+        klass = row.class_name
+        shutil.copy(
+            os.path.join(data_dir, "species-info", klass, "current", f"{taxid}.geojson"),
+            output_dir
+        )
+        shutil.copy(
+            os.path.join(data_dir, "aohs", "current", klass, f"{taxid}_all.tif"),
+            output_dir
+        )
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Collected range/AOH for species that failed validation")
+    parser.add_argument(
+        '--model_results',
+        type=str,
+        help="directory with taxa folders of species info",
+        required=True,
+        dest="model_results_path"
+    )
+    parser.add_argument(
+        '--datadir',
+        type=str,
+        help="directory for results",
+        required=True,
+        dest="data_dir",
+    )
+    parser.add_argument(
+        '--output',
+        type=str,
+        help="name of output directory",
+        required=True,
+        dest="output"
+    )
+    args = parser.parse_args()
+    collect_validation_data(args.model_results_path, args.data_dir, args.output)
+
+if __name__ == "__main__":
+    main()
